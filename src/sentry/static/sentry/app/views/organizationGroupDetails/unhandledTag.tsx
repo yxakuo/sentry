cf6e182334867dd10bea75ae85c43082b8723b50
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-11-20 00:26:13 +0100

    removed unhandled tag icon

--- src/sentry/static/sentry/app/views/organizationGroupDetails/unhandledTag.tsx
+++ src/sentry/static/sentry/app/views/organizationGroupDetails/unhandledTag.tsx
@@ -6,20 +6,19 @@ import space from 'app/styles/space';
 import Tag from 'app/components/tag';
 import Feature from 'app/components/acl/feature';
 import Tooltip from 'app/components/tooltip';
-import {IconSubtract} from 'app/icons';
 
 // TODO(matej): remove "unhandled-issue-flag" feature flag once testing is over (otherwise this won't ever be rendered in a shared event)
-const UnhandledTag = () => (
-  <Feature features={['unhandled-issue-flag']}>
-    <TagWrapper>
-      <Tooltip title={t('An unhandled error was detected in this Issue.')}>
-        <Tag type="error" icon={<IconSubtract size="xs" color="red200" isCircled />}>
-          {t('Unhandled')}
-        </Tag>
-      </Tooltip>
-    </TagWrapper>
-  </Feature>
-);
+function UnhandledTag() {
+  return (
+    <Feature features={['unhandled-issue-flag']}>
+      <TagWrapper>
+        <Tooltip title={t('An unhandled error was detected in this Issue.')}>
+          <Tag type="error">{t('Unhandled')}</Tag>
+        </Tooltip>
+      </TagWrapper>
+    </Feature>
+  );
+}
 
 const TagWrapper = styled('div')`
   margin-right: ${space(1)};
